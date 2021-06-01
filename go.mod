92f3f5266330e6689a525314d2304214decfc2c1
Rebeca Calazans <rebecac.brito@gmail.com>
2021-06-01 13:29:53 -0700

    Add connection to db

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/IMEplusplus/dont-panic-api
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/rs/zerolog v1.22.0
+	github.com/supplyon/gremcos v0.1.7
+)
--- main.go
+++ main.go
@@ -4,8 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+
+	"github.com/rs/zerolog"
+	gremcos "github.com/supplyon/gremcos"
+	"github.com/supplyon/gremcos/api"
 )
 
 type Subject struct {
@@ -17,22 +23,72 @@ type Subject struct {
 }
 
 var subjects = []Subject{}
+var cosmos gremcos.Cosmos
+var logger zerolog.Logger
+var err error
 
 func main() {
+
+	host := os.Getenv("CDB_HOST")
+	username := os.Getenv("CDB_USERNAME")
+	password := os.Getenv("CDB_KEY")
+
+	fmt.Println(host)
+	fmt.Println(username)
+	fmt.Println(password)
+
+	logger = zerolog.New(os.Stdout).Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: zerolog.TimeFieldFormat}).With().Timestamp().Logger()
+	cosmos, err = gremcos.New(host,
+		gremcos.WithAuth(username, password),
+		gremcos.WithLogger(logger),
+		gremcos.NumMaxActiveConnections(10),
+		gremcos.ConnectionIdleTimeout(time.Second*30),
+		gremcos.MetricsPrefix("myservice"),
+	)
+
+	if err != nil {
+		return
+	}
+
 	r := mux.NewRouter()
-	usersR := r.PathPrefix("/subject").Subrouter()
+	usersR := r.PathPrefix("/api/subject").Subrouter()
 	usersR.Path("").Methods(http.MethodGet).HandlerFunc(getAllSubjects)
 	usersR.Path("").Methods(http.MethodPost).HandlerFunc(createSubject)
 	usersR.Path("/{id}").Methods(http.MethodGet).HandlerFunc(getSubjectByID)
 	usersR.Path("/{id}").Methods(http.MethodPut).HandlerFunc(updateSubject)
 	usersR.Path("/{id}").Methods(http.MethodDelete).HandlerFunc(deleteSubject)
 	fmt.Println("Start listening")
-	fmt.Println(http.ListenAndServe("", r))
+
+	listenAddr := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+	}
+
+	fmt.Println(http.ListenAndServe(listenAddr, r))
 }
 
 func getAllSubjects(w http.ResponseWriter, r *http.Request) {
+
+	g := api.NewGraph("g")
+	query := g.V()
+
+	res, err := cosmos.ExecuteQuery(query)
+
+	if err != nil {
+		logger.Error().Err(err).Msg("Failed to execute a gremlin command")
+		return
+	}
+
+	responses := api.ResponseArray(res)
+	values, err := responses.ToValues()
+	if err == nil {
+		logger.Info().Msgf("Received Values: %v", values)
+	}
+
+	println(responses)
+
 	w.Header().Add("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(subjects); err != nil {
+	if err := json.NewEncoder(w).Encode(responses); err != nil {
 		fmt.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 	}
