44c30da0f62cf1e480589d1b6c72065bc4eb18fc
Rebeca Calazans <rebecac.brito@gmail.com>
2021-08-21 15:57:42 -0700

    Revert "Change 'model' to 'modelStorage' and add 'modelAPI'"

--- app/dbOperations/subjects.go
+++ app/dbOperations/subjects.go
@@ -8,10 +8,10 @@ import (
 	"github.com/supplyon/gremcos/api"
 	"github.com/supplyon/gremcos/interfaces"
 
-	storageModel "github.com/imeplusplus/dont-panic-api/app/model/storage"
+	"github.com/imeplusplus/dont-panic-api/app/model"
 )
 
-func GetSubjects(cosmos gremcos.Cosmos) ([]storageModel.Subject, error) {
+func GetSubjects(cosmos gremcos.Cosmos) ([]model.Subject, error) {
 	g := api.NewGraph("g")
 	query := g.V().HasLabel("subject")
 
@@ -34,8 +34,8 @@ func GetSubjects(cosmos gremcos.Cosmos) ([]storageModel.Subject, error) {
 	return subjects, nil
 }
 
-func GetSubjectByName(cosmos gremcos.Cosmos, name string) (storageModel.Subject, error) {
-	var subject storageModel.Subject
+func GetSubjectByName(cosmos gremcos.Cosmos, name string) (model.Subject, error) {
+	var subject model.Subject
 	g := api.NewGraph("g")
 	query := g.V().HasLabel("subject").Has("name", name)
 
@@ -49,34 +49,33 @@ func GetSubjectByName(cosmos gremcos.Cosmos, name string) (storageModel.Subject,
 	return getSubjectFromResponse(res)
 }
 
-func CreateSubject(cosmos gremcos.Cosmos, subject storageModel.Subject) (storageModel.Subject, error) {
+func CreateSubject(cosmos gremcos.Cosmos, subject model.Subject) (model.Subject, error) {
 	_, err := GetSubjectByName(cosmos, subject.Name)
 
 	if err == nil {
-		return storageModel.Subject{}, errors.New("There is already a subject with name " + subject.Name)
+		return model.Subject{}, errors.New("There is already a subject with name " + subject.Name)
 	}
 
 	g := api.NewGraph("g")
 
 	query := g.AddV("subject").Property("partitionKey", "subject")
-
 	query = addVertexProperties(query, subject)
 
 	res, err := cosmos.ExecuteQuery(query)
 	if err != nil {
 		fmt.Println("Failed to execute a gremlin command " + query.String())
 		// logger.Error().Err(err).Msg("Failed to execute gremlin command")
-		return storageModel.Subject{}, err
+		return subject, err
 	}
 
 	return getSubjectFromResponse(res)
 }
 
-func UpdateSubject(cosmos gremcos.Cosmos, subject storageModel.Subject, name string) (storageModel.Subject, error) {
+func UpdateSubject(cosmos gremcos.Cosmos, subject model.Subject, name string) (model.Subject, error) {
 	oldSubject, err := GetSubjectByName(cosmos, name)
 
 	if err != nil {
-		return storageModel.Subject{}, errors.New("There is no subject with name " + oldSubject.Name)
+		return model.Subject{}, errors.New("There is no subject with name " + oldSubject.Name)
 	}
 
 	g := api.NewGraph("g")
@@ -86,7 +85,7 @@ func UpdateSubject(cosmos gremcos.Cosmos, subject storageModel.Subject, name str
 	if err != nil {
 		fmt.Println("Failed to execute a gremlin command " + query.String())
 		//logger.Error().Err(err).Msg("Failed to execute a gremlin command")
-		return storageModel.Subject{}, err
+		return subject, err
 	}
 
 	return getSubjectFromResponse(res)
@@ -101,7 +100,7 @@ func DeleteSubject(cosmos gremcos.Cosmos, name string) error {
 	return err
 }
 
-func addVertexProperties(vertex interfaces.Vertex, subject storageModel.Subject) interfaces.Vertex {
+func addVertexProperties(vertex interfaces.Vertex, subject model.Subject) interfaces.Vertex {
 	vertex = vertex.
 		Property("name", subject.Name).
 		Property("difficulty", subject.Difficulty).
@@ -118,33 +117,38 @@ func addVertexProperties(vertex interfaces.Vertex, subject storageModel.Subject)
 	return vertex
 }
 
-func getSubjectFromResponse(res []interfaces.Response) (storageModel.Subject, error) {
-	var subject storageModel.Subject
+func getSubjectFromResponse(res []interfaces.Response) (model.Subject, error) {
+	var subject model.Subject
 	response := api.ResponseArray(res)
-	vertices, _ := response.ToVertices()
+	vertices, err := response.ToVertices()
 
 	if len(vertices) == 0 {
-		return subject, errors.New("there is no vertex in the response")
+		return subject, errors.New("There is no vertex in the response")
 	}
 
-	subject = vertexToSubject(vertices[0])
+	subject, err = vertexToSubject(vertices[0])
+	if err != nil {
+		return subject, err
+	}
 
 	return subject, nil
 }
 
-func verticesToSubjects(vertices []api.Vertex) []storageModel.Subject {
-	subjects := []storageModel.Subject{}
+func verticesToSubjects(vertices []api.Vertex) []model.Subject {
+	subjects := []model.Subject{}
 
 	for _, v := range vertices {
-		subject := vertexToSubject(v)
-		subjects = append(subjects, subject)
+		subject, err := vertexToSubject(v)
+		if err == nil {
+			subjects = append(subjects, subject)
+		}
 	}
 
 	return subjects
 }
 
-func vertexToSubject(vertex api.Vertex) storageModel.Subject {
-	var subject storageModel.Subject
+func vertexToSubject(vertex api.Vertex) (model.Subject, error) {
+	var subject model.Subject
 
 	subject.Id = vertex.ID
 
@@ -158,5 +162,5 @@ func vertexToSubject(vertex api.Vertex) storageModel.Subject {
 		subject.References = append(subject.References, p.Value.AsString())
 	}
 
-	return subject
+	return subject, nil
 }
